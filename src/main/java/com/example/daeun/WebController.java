b7e808fcf1a38331650dcaf35152c20b3d8945cd
jeongdaeun98 <uneap@sk.com>
2021-02-10 14:43:40 +0900

    config: add dockerFile

--- src/main/java/com/example/daeun/WebController.java
+++ src/main/java/com/example/daeun/WebController.java
@@ -2,6 +2,7 @@ package com.example.daeun;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 
 @Controller
@@ -9,6 +10,12 @@ public class WebController {
     @GetMapping("/")
     @ResponseBody
     public String main(){
+        return "<a href=\"https://github.com/\">github.com</a>";
+    }
+
+    @PostMapping("/")
+    @ResponseBody
+    public String mainPost() {
         return "<a href=\"https://github.com/\">github.com</a>";
     }
 }
